723ee856362d136920ceb0f8b9d87fccd9dd6659
Alexander Ryzhov <a1exander@inbox.ru>
2022-09-01 10:53:19 +0300

    Add install prerequisites packages and test for it

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,15 @@
+import pytest
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+@pytest.mark.parametrize("pkgs", [
+    "ca-certificates",
+    "curl",
+    "gnupg",
+    "lsb-release"
+])
+def test_prerequisites_packages(host, pkgs):
+    pkg = host.package(pkgs)
+    assert pkg.is_installed
